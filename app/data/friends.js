0af88b41d73da00bc9a1b507ec34e65ce207b9fc
dps0610 <dps0610@hotmail.com>
2019-08-07 17:16:59 -0400

    added server logic, routes, some data.

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var friends = [
+    {
+        name: "Jim",
+        img: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzEwNC84MTkvb3JpZ2luYWwvY3V0ZS1raXR0ZW4uanBn",
+        scores: [
+            "5",
+            "1",
+            "3",
+            "4",
+            "2"
+        ]
+    },
+    {
+        name: "Pam",
+        img: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzEwNC84MTkvb3JpZ2luYWwvY3V0ZS1raXR0ZW4uanBn",
+        scores: [
+            "4",
+            "1",
+            "2",
+            "4",
+            "5"
+        ]
+    }
+]
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var PORT = 8080;
+
+//use this tomorrow!
+// var PORT = process.env.PORT || 8080;
+
+
+
+//so your stuff can interpret the data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Router
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
